1b13a8fa910d659020b3206038339ab26b1f2910
Andreas Schröpfer <20220983+as27@users.noreply.github.com>
2023-01-14 12:47:27 +0100

    Logout added

--- auth.go
+++ auth.go
@@ -9,12 +9,47 @@ import (
 	"strings"
 )
 
+// LogIn authenticates the user with the pCloud API.
+// With the given AuthMethod the auth-token is requested.
 func LogIn(user, pass string) error {
 	token, err := getAuthToken(user, pass)
 	if err != nil {
 		return fmt.Errorf("LogIn getAuthToken: %w", err)
 	}
 	authToken = token
+	return nil
+}
+
+// LogOut logs the user out of the pCloud API. When the
+// logout request is successful the authToken is set to "".
+func LogOut() error {
+	u, err := url.Parse(HostURL + "/logout")
+	if err != nil {
+		return fmt.Errorf("LogOut url.Parse: %w", err)
+	}
+	q := u.Query()
+	q.Add("auth", authToken)
+	u.RawQuery = q.Encode()
+	resp, err := HTTPClient.Get(u.String())
+	if err != nil {
+		return fmt.Errorf("LogOut http.Get: %w", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("LogOut status code: %d\n%s", resp.StatusCode, u.String())
+	}
+	type logoutResponse struct {
+		Result      int  `json:"result"`
+		AuthDeleted bool `json:"auth_deleted"`
+	}
+	lr := &logoutResponse{}
+	if err := json.NewDecoder(resp.Body).Decode(lr); err != nil {
+		return fmt.Errorf("LogOut json.Decode: %w", err)
+	}
+	if !lr.AuthDeleted {
+		return fmt.Errorf("LogOut at server not succsessfull")
+	}
+	authToken = ""
 	return nil
 }
 
